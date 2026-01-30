f6af47e3e84cd37b2a46cc4246dfe8cbb5de9de3
Andrews <131870616+andrews326@users.noreply.github.com>
2026-01-30 12:11:26 +0530

    Update AMI and key name for AWS instances

--- Instance-terraform/main.tf
+++ Instance-terraform/main.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "Jenkins-sg" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-073130f74f5ffb161"  #change your ami value according to your aws instance
+  ami                    = "ami-019715e0d74f695be"  #change your ami value according to your aws instance
   instance_type          = "t3.large"
-  key_name               = "linux-server-kp"
+  key_name               = "mumbai-region-key"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./script.sh", {})
 
@@ -45,9 +45,9 @@ resource "aws_instance" "web" {
   }
 }
 resource "aws_instance" "web2" {
-  ami                    = "ami-073130f74f5ffb161" #change your ami value according to your aws instance 
+  ami                    = "ami-019715e0d74f695be" #change your ami value according to your aws instance 
   instance_type          = "t3.medium"
-  key_name               = "linux-server-kp"
+  key_name               = "mumbai-region-key"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   tags = {
     Name = "Monitering via grafana"
